9fd7be07f491267676043661ead867b9432a2590
Wasa C <gie68i@gmail.com>
2017-04-25 00:43:12 +0700

    Added bot with sdk file

---
+++ bot_with_sdk.php
@@ -0,0 +1,6 @@
+<?php
+$access_token = getenv('ACCESS_TOKEN'); // get from Heroku Config Vars
+$channel_secret = getenv('CHANNEL_SECRET'); // get from Heroku Config Vars
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channel_secret]);
