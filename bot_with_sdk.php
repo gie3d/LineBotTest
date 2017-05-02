a15d7863caae0c297ff2061136cbd186ad2a5ead
Wasa C <gie68i@gmail.com>
2017-05-02 07:30:13 +0700

    Test bot with sdk

--- bot_with_sdk.php
+++
@@ -1,8 +0,0 @@
-<?php
-$access_token = getenv('ACCESS_TOKEN'); // get from Heroku Config Vars
-$channel_secret = getenv('CHANNEL_SECRET'); // get from Heroku Config Vars
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channel_secret]);
-
-echo 'test';
---
+++ bot_without_sdk.php
@@ -0,0 +1,87 @@
+<?php
+$access_token = getenv('ACCESS_TOKEN'); // get from Heroku Config Vars
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			if ($text == 'nong') {
+				$messages = [
+					'type' => 'imagemap',
+					'baseUrl' => 'https://qonlinebot.herokuapp.com/assets/image1.jpg/700',
+					'altText' => 'This is an imagemap',
+					'baseSize' => [
+						'width' => 1040,
+						'height' => 500
+					],
+					'actions' => [[
+						'type' => 'uri',
+						'linkUri' => 'https://www.facebook.com',
+						'area' => [
+							'x' => 0,
+							'y' => 0,
+							'width' => 500,
+							'height' => 500
+						]
+					], [
+						'type' => 'uri',
+						'linkUri' => 'https://www.facebook.com',
+						'area' => [
+							'x' => 500,
+							'y' => 0,
+							'width' => 500,
+							'height' => 500
+						]
+					], [
+						'type' => 'message',
+						'text' => 'hello',
+						'area' => [
+							'x' => 0,
+							'y' => 0,
+							'width' => 500,
+							'height' => 500
+						]
+					]]
+				];
+			}
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
